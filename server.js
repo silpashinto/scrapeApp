c58bb8cd1a7891f0a911d3cdfb1ef37f73959a15
Silpa <silpashinto@gmail.com>
2019-03-30 12:10:47 -0400

    fixes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
